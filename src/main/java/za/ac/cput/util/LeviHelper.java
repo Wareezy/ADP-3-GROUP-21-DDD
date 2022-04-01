4ac6c8c92b2f4ce7c187107b70d277223d506380
Levi <218250142@mycput.ac.za>
2022-04-01 15:33:24 +0300

    commit 4

--- src/main/java/za/ac/cput/util/LeviHelper.java
+++ src/main/java/za/ac/cput/util/LeviHelper.java
@@ -2,7 +2,7 @@ package za.ac.cput.util;
 
 import java.util.UUID;
 
-public class Helper {
+public class LeviHelper {
     public static String tableID(){
         return UUID.randomUUID().toString();
     }
